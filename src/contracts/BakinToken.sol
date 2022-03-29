d1d976cbe3ae990b64b43ff770f6fc2acd0b4434
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2022-03-29 15:26:32 -0500

    files importing correctly

--- src/contracts/BakinToken.sol
+++ src/contracts/BakinToken.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
-
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BakinToken is ERC20 {
--- src/contracts/StakinToken.sol
+++ src/contracts/StakinToken.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
-
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
